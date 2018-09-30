1f2f213ddc17608eb8e9f0ca36f3dba43e7d9940
Wu, Meng Ju <mew177@pitt.edu>
2018-09-30 17:31:04 -0400

    LeetCode No.507
    Perfect Number

---
+++ LeetCode/507. Perfect Number.py
@@ -0,0 +1,21 @@
+"""
+    Question:
+    1. How large will the number possible be?
+    2. Is 1 a perfect number?
+    3.
+
+    Idea:
+    1. Check dividers less than sqrt(num), and sum them. (Terrible Time Complexity?)
+
+"""
+
+num = 28
+
+if num <= 1:
+    print(False)
+s = 1
+for i in range(2, int(num ** 0.5) + 1):
+    if num % i == 0:
+        s += i
+        s += num // i
+print(num == s)
