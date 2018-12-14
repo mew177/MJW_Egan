7fd37fb697717095eb9c6c86aca366634a075e61
Wu, Meng Ju <mew177@pitt.edu>
2018-12-14 13:50:24 -0500

    No. 155
    Min Stack

--- LeetCode/155. Min Stack.py
+++ LeetCode/155. Min Stack.py
@@ -1,3 +1,34 @@
+"""
+    Main Idea: All function with TC O(1)
+    Every input x: stored as [ smallest x after this position, x ]
+
+"""
+
+class MinStack(object):
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        if self.stack:
+            self.stack.append(min(self.stack[-2], x))
+        else:
+            self.stack.append(x)
+        self.stack.append(x)
+
+    def pop(self):
+        if self.stack:
+            self.stack.pop()
+            self.stack.pop()
+
+    def top(self):
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self):
+        if self.stack:
+            return self.stack[-2]
+
+"""
 class MinStack(object):
     def __init__(self):
         self.stack = []
@@ -21,13 +52,14 @@ class MinStack(object):
 
     def getMin(self): # constant time
         return self.order[0]
+"""
 
 stack = MinStack()
 print(stack.push(-2))
 print(stack.push(0))
 print(stack.push(-3))
 print(stack.getMin()) # -3
-print(stack.pop()) # -3
+print(stack.pop())
 print(stack.top()) # 0
 print(stack.getMin()) # -2
 
